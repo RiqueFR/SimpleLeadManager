70c59eb07a7deb1b1b45ac36e19b3847c9b5b536
Henrique Faria Ribeiro <rique.far.rib@gmail.com>
2022-04-09 16:53:39 -0300

    Rendering the info in the Local Storage on the Leads page

--- src/components/Lead.js
+++ src/components/Lead.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
+import { getDataFromLocalStorage } from "../services/Store";
 
 function initialState() {
 	return { lead: [],
@@ -16,6 +17,12 @@ const Lead = () => {
 	const [values, setValues] = useState(initialState);
 	const navigate = useNavigate();
 
+	const leads = getDataFromLocalStorage('lead');
+	const leadsComponents = [];
+	for(let index in leads) {
+		leadsComponents.push(<p key={index}>{leads[index].name}</p>)
+	}
+
 	const onCLick = (event) => {
 		navigate("/lead/new");
 	};
@@ -24,6 +31,7 @@ const Lead = () => {
 		<div className="lead">
 			<button onClick={onCLick}>Novo Lead (+)</button>
 			<div className="table">
+				{leadsComponents}
 			</div>
 		</div>
 	);
