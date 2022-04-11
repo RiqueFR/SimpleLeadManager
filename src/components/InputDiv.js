7287611c1938dd49f1a60226ecff2a57e6f885a0
Henrique Faria Ribeiro <rique.far.rib@gmail.com>
2022-04-11 16:13:54 -0300

    Refactor: remove logic from view

--- src/components/InputDiv.js
+++ src/components/InputDiv.js
@@ -1,17 +1,11 @@
 import React from "react";
 
 const InputDiv = (props) => {
-	const errors = props.errors;
-	let errors_labels = [];
-	for (let index in errors) {
-		errors_labels.push(<label className="errors" key={index}>{errors[index]}</label>);
-	}
-
 	return (
 		<div className="sign-input">
 			<span>{props.text}</span>
 			<input id={props.id} type={props.type} name={props.name} onChange={props.onChange} value={props.value} />
-			{errors_labels}
+			{props.errors.map((erro, index) => (<label className="errors" key={index}>{erro}</label>))}
 		</div>
 	);
 }
