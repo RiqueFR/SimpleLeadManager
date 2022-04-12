72ea6ae718b7a320739f4ac48fd235cb42fbe3ae
Henrique Faria Ribeiro <rique.far.rib@gmail.com>
2022-04-11 21:00:58 -0300

    Add styled button to SignUp

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -11,7 +11,7 @@ const SignUp = ({ data, onSubmit, onChange, toast }) => {
 				<InputDiv text="Usuário" id="user" type="text" name="user" onChange={onChange} value={data.user} errors={data.errors.username} />
 				<InputDiv text="Password" id="password" type="password" name="password" onChange={onChange} value={data.password} errors={data.errors.password} />
 				<InputDiv text="Confirmação Password" id="conf-password" type="password" name="confPassword" onChange={onChange} value={data.confPassword} errors={data.errors.confPassword} />
-				<button>Registrar</button>
+				<button className="BUTTON_MAT">Registrar</button>
 			</form>
 			<ToastContainer autoClose={10000} />
 		</div>
